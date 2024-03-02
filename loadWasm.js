b3f9620e57edf5d38ef959a2cec9f5f293f76686
坂本一弥 <sskazuya1130@gmail.com>
2024-03-03 00:02:33 +0900

    chore: wasm-pack

--- loadWasm.js
+++ loadWasm.js
@@ -1,33 +1,69 @@
-const imports = {
-  env: {
-    date_now: Date.now,
-  },
+import * as wasm from "./pkg/wasm_dev_book_hello_wasm.js";
+
+const HOBBIES = {
+  DEVELOPMENT: "開発",
+  MUSIC: "音楽",
+  SPORTS: "スポーツ",
+  TRAVEL: "旅行",
+  READING: "読書",
+  GAMING: "ゲーム",
+  ANIME: "アニメ",
 };
-const WASM_PATH =
-  "./target/wasm32-unknown-unknown/release/wasm_dev_book_hello_wasm.wasm";
 
 const formatTimestamp = (timestamp) => {
-  const date = new Date(timestamp);
+  const date = new Date(timestamp * 1000);
   return date.toLocaleString();
 };
 
 const toUint32 = (num) => num >>> 0;
 
-const useWasm = async () => {
-  try {
-    const response = await fetch(WASM_PATH);
-    const bytes = await response.arrayBuffer();
-    const results = await WebAssembly.instantiate(bytes, imports);
+const useWasmFunctions = () => {
+  console.log(wasm.add(1, 2));
+  console.log(formatTimestamp(wasm.get_timestamp()));
+  console.log(toUint32(wasm.rand()));
+  console.log(
+    `String length: ${wasm.get_string_length("Hello, WebAssembly!")}`
+  );
+};
 
-    const { add, get_timestamp, rand } = results.instance.exports;
-    console.log(add(1, 2));
-    console.log(formatTimestamp(get_timestamp()));
-    console.log(toUint32(rand()));
+const fetchAndProcessData = async () => {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const json = await response.json();
+    console.log("Original JSON:", json);
+
+    const modifiedJson = wasm.modify_response(json.title, json.completed);
+    console.log("Modified JSON:", modifiedJson);
   } catch (error) {
-    console.error("Wasm loading and use failed:", error);
+    console.error("Error fetching and processing data:", error);
   }
 };
 
-(() => {
-  useWasm();
-})();
+const processUserInput = (inputText) => {
+  console.log(`Word count: ${wasm.count_words(inputText)}`);
+};
+
+const onSearhUser = () => {
+  const param = {
+    name: "ka",
+    sex: 1,
+    age: 28,
+    hobby: HOBBIES.DEVELOPMENT,
+  };
+
+  console.log(
+    wasm.search_by_criteria(param.name, param.sex, param.age, param.hobby)
+  );
+};
+
+const main = async () => {
+  await wasm.default();
+  useWasmFunctions();
+  await fetchAndProcessData();
+  processUserInput("This is a sample text for WebAssembly.");
+  onSearhUser();
+};
+
+main().catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,36 @@
-#[no_mangle]
-// コンパイラにこの関数の名前を変更しないよう指示
+use wasm_bindgen::prelude::*;
+use tinymt::tinymt32;
+use wasm_bindgen::JsValue;
+use serde::{Serialize, Deserialize};
+
+/**
+ * JavaScriptのDate.now関数をRustから呼び出す
+ */
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = Date)]
+    fn now() -> f64;
+}
+/**
+ * 数値加算関数
+ */
+#[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
-
-// index.html > imports.env.date_now を参照できるようにする
-extern {
-    fn date_now() -> f64;
-}
-
-#[no_mangle]
+/**
+ * 現在のタイムスタンプを取得
+ */
+#[wasm_bindgen]
 pub fn get_timestamp() -> f64 {
     unsafe {
-        date_now()
+        now()
     }
 }
-
-use tinymt::tinymt32;
-
-#[no_mangle]
+/**
+ * 乱数生成関数
+ */
+#[wasm_bindgen]
 pub fn rand() -> u32 {
     let param = tinymt32::Param {
         mat1: 0x8F7011EE,
@@ -27,4 +39,77 @@ pub fn rand() -> u32 {
     };
     let seed = 1;
     tinymt32::from_seed(param, seed).gen()
+}
+/**
+ * 文字列長さ
+ */
+#[wasm_bindgen]
+pub fn get_string_length(input: &str) -> usize {
+    input.len()
+}
+/**
+ * Responseを整形
+ */
+#[wasm_bindgen]
+pub fn modify_response(title: &str, completed: bool) -> JsValue {
+    let modify_title = format!("Modified: {}", title);
+    let modify_status = !completed; // 完了状態反転
+
+    JsValue::from_serde(&serde_json::json!({
+        "title": modify_title,
+        "completed": modify_status,
+    })).unwrap()
+}
+/**
+ * 入力文字列カウント
+ */
+#[wasm_bindgen]
+pub fn count_words(input: &str) -> usize {
+    input.split_whitespace().count()
+}
+/**
+ * 検索の機構を作る
+ */
+#[derive(Serialize, Deserialize)]
+pub struct Person {
+    id: u32,
+    name: String,
+    sex: u8, // 1: 男性, 2: 女性, 3: 不明
+    age: u8,
+    hobbies: Vec<String>,
+}
+
+// 趣味の定数リスト
+const HOBBIES_DEVELOPMENT: &str = "開発";
+const HOBBIES_MUSIC: &str = "音楽";
+const HOBBIES_SPORTS: &str = "スポーツ";
+const HOBBIES_TRAVEL: &str = "旅行";
+const HOBBIES_READING: &str = "読書";
+const HOBBIES_GAMING: &str = "ゲーム";
+
+#[wasm_bindgen]
+pub fn search_by_criteria(
+    name_query: &str, 
+    sex_query: u8,
+    age_query: Option<u8>,
+    hobby_query: Option<String>
+) -> JsValue {
+    let people = vec![
+        Person { id: 1, name: "kazuya".to_string(), sex: 1, age: 28, hobbies: vec![HOBBIES_DEVELOPMENT.to_string(), HOBBIES_MUSIC.to_string()] },
+        Person { id: 2, name: "tarou".to_string(), sex: 1, age: 30, hobbies: vec![HOBBIES_SPORTS.to_string(), HOBBIES_GAMING.to_string()] },
+        Person { id: 3, name: "riko".to_string(), sex: 2, age: 30, hobbies: vec![HOBBIES_READING.to_string(), HOBBIES_MUSIC.to_string()] },
+        Person { id: 4, name: "kinako".to_string(), sex: 3, age: 2, hobbies: vec![HOBBIES_TRAVEL.to_string()] },
+        Person { id: 5, name: "kazuma".to_string(), sex: 1, age: 28, hobbies: vec![HOBBIES_SPORTS.to_string(), HOBBIES_DEVELOPMENT.to_string()] },
+    ];
+
+    let result: Vec<&Person> = people.iter()
+        .filter(|&person| 
+            person.name.to_lowercase().contains(&name_query.to_lowercase()) &&
+            person.sex == sex_query &&
+            age_query.map_or(true, |age| person.age == age) &&
+            hobby_query.as_ref().map_or(true, |hobby| person.hobbies.iter().any(|h| h.to_lowercase() == hobby.to_lowercase()))
+        )
+        .collect();
+
+    JsValue::from_serde(&result).unwrap()
 }
